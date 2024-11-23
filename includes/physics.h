da3f45cd48a49a063b6d4d3d975dcb5a52e1cd31
Jules Reix Charat <jureix-c@z2r12p4.42lyon.fr>
2024-11-23 21:47:02 +0100

    feat: basic physics engine

--- includes/physics.h
+++ includes/physics.h
@@ -16,16 +16,16 @@
 # define MIN_SPEED 0f
 # define MAX_SPEED 10f
 
-# define MIN_ACCELERATION 0f
+# define MIN_ACCELERATION -10f
 # define MAX_ACCELERATION 10f
 
 // MOVEMENT
 float	ft_accelerate(float velocity, float acceleration, float delta_time);
-void	ft_move(float *x, float *y, float velocity, float angle, float delta_time);
+void	ft_move(float *y, float *x, float velocity, float angle, float delta_time);
 
 // COLLISION
 bool	ft_ranges_overlap(float min1, float max1, float min2, float max2);
-bool	ft_rectangles_intersect(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2);
-bool	ft_segments_intersect(float y1, float x1, float y2, float x2);
+bool	ft_rectangles_intersect(float y1, float x1, float h1, float w1, float y2, float x2, float h2, float w2);
+bool	ft_segments_intersect(float a_y1, float a_x1, float a_y2, float a_x2, float b_y1, float b_x1, float b_y2, float b_x2);
 
 #endif
--- sources/physics/movement.c
+++ sources/physics/movement.c
@@ -6,8 +6,19 @@
 /*   By: jureix-c <jureix-c@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 06:01:48 by jureix-c          #+#    #+#             */
-/*   Updated: 2024/11/23 06:01:49 by jureix-c         ###   ########.fr       */
+/*   Updated: 2024/11/23 20:52:57 by jureix-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <math.h>
 
+float	ft_accelerate(float velocity, float acceleration, float delta_time)
+{
+	return (velocity + acceleration * delta_time);
+}
+
+void	ft_move(float *y, float *x, float velocity, float angle, float delta_time)
+{
+	*y += velocity * sinf(angle) * delta_time;
+	*x += velocity * cosf(angle) * delta_time;
+}
