b5d1df04449e7dbf17ffcb96233bf97e508a322d
kporceil <163202211+Natox10@users.noreply.github.com>
2024-11-24 19:33:12 +0100

    feat: collision between laser

--- sources/scenes/game/entity/entity_player_laser.c
+++ sources/scenes/game/entity/entity_player_laser.c
@@ -28,7 +28,7 @@ void	*init_entity_player_laser(t_game_data *game_data)
 	entity->perforation = 1;
 	entity->damage = 100;
 	entity->color = COLOR_SET_BLUE;
-	entity->icon = '|';
+	entity->icon = '8';
 	return ((void *)entity);
 }
 
@@ -46,22 +46,13 @@ void	handle_entity_player_laser_collisions(t_entity *entity, t_game_data *game_d
 	t_entity	*entity2;
 
 	entity2 = game_data->entities;
-	while (entity && entity2)
+	while (entity2)
 	{
 		switch (entity2->type)
 		{
-			case ENTITY_ENEMY_SHIP:
-				if (ft_entities_collide(entity, entity2))
-				{
-					((t_entity_enemy_ship *)entity2->data)->health -= ((t_entity_player_laser *)entity->data)->damage;
-					if (((t_entity_player_laser *)entity->data)->perforation <= 0)
-					{
-						entity_delone(entity);
-						entity = NULL;
-					}
-					else
-						((t_entity_player_laser *)entity->data)->perforation--;
-				}
+			case ENTITY_ENEMY_LASER:
+				if (((int)entity2->next_y == (int)entity->next_y && (int)entity2->next_x == (int)entity->next_x) || ((int)entity2->y == (int)entity->y && (int)entity2->x == (int)entity->x))
+					((t_entity_player_laser *)entity->data)->perforation = 0;
 				break;
 			default:
 				break;
